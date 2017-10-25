ccf739e171d86b0fd7b20ee3fd9ab2b32ec8a9ff
Allajah <r1o.ryoma.abe@gmail.com>
2017-10-26 01:37:52 +0900

    Create example

---
+++ example/index.js
@@ -0,0 +1,12 @@
+colorpack = require('colorpack');
+console.log(colorpack.colorpack('#2A98C5'))
+// =>
+// { r: 42,
+//   g: 152,
+//   b: 197,
+//   a: 0,
+//   hex: '#2A98C5',
+//   h: 197.4193548387097,
+//   s: 0.787,
+//   v: 0.773,
+//   integer: 2791621 }
